d007363997d84dfeba7bf9077c07b06e0a5b82b0
Miloš Lipničan <77415998+Lipnicanmilos@users.noreply.github.com>
2021-09-08 10:51:38 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function(){
+
+    "use strict";
+
+    $(window).scroll(function(){
+        var top = $(window).scrollTop();
+            if(top>=60){
+                $("header").addClass('secondary');
+            }
+            else 
+                if($("header").hasClass('secondary')){
+                    $("header").removeClass('secondary');
+                } 
+    });
+});
